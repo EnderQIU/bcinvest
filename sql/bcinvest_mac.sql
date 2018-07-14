ee74b5289f75f474ad004a2eaea83642f5518572
enderqiu <a934560824@163.com>
2018-07-14 19:30:12 +0800

    add type to report

--- sql/bcinvest_mac.sql
+++ sql/bcinvest_mac.sql
@@ -1,272 +1,109 @@
-﻿# Host: localhost  (Version 5.7.16-log)
-# Date: 2018-07-08 10:25:39
-# Generator: MySQL-Front 6.0  (Build 2.20)
-
-USE bcinvest;
-
-#
-# Structure for table "authorization"
-#
-
-DROP TABLE IF EXISTS `Authorization`;
-CREATE TABLE `Authorization` (
-  `Name` varchar(128),
-  `Type` int(11),
-  `AccountNum` varchar(128),
-  `Token` varchar(128) UNIQUE,
-  `Password` varchar(128),
-  PRIMARY KEY (`AccountNum`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-#
-# Data for table "authorization"
-#
-
-
-#
-# Structure for table "company"
-#
-
-DROP TABLE IF EXISTS `Company`;
-CREATE TABLE `Company` (
-  `AccountNum` varchar(128),
-  `Name` varchar(128),
-  `TelNum` varchar(128) DEFAULT NULL,
-  `EmailAddress` varchar(128) UNIQUE,
-  `Token` varchar(128) UNIQUE,
-  `Credit` bigint(20),
-  `Status` VARCHAR(128) NULL DEFAULT "unapplied" CHECK (VALUE IN ("unapplied", "checking_1", "checking_2", "unpassed",
-  "passed")),
- #`company` varchar(128) Default null,
-  PRIMARY KEY (`AccountNum`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-#
-# Data for table "company"
-#
-
-
-
-#
-# Structure for table "accesstoken"
-#
-
-DROP TABLE IF EXISTS `Accesstoken`;
-CREATE TABLE `Accesstoken` (
-  `AccessToken` varchar(512),
-  `ExpiryTime` datetime,
+﻿CREATE TABLE `Accesstoken` (
+  `AccessToken` varchar(512) DEFAULT NULL,
+  `ExpiryTime` datetime DEFAULT NULL,
   `AccountNum` varchar(128) DEFAULT '',
   KEY `AccountNum` (`AccountNum`),
-  CONSTRAINT `accesstoken_ibfk_1` FOREIGN KEY (`AccountNum`) REFERENCES `Company` (`AccountNum`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT `accesstoken_ibfk_1` FOREIGN KEY (`AccountNum`) REFERENCES `company` (`accountnum`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
-#
-# Data for table "accesstoken"
-#
+CREATE TABLE `Authorization` (
+  `Name` varchar(128) DEFAULT NULL,
+  `Type` int(11) DEFAULT NULL,
+  `AccountNum` varchar(128) NOT NULL,
+  `Token` varchar(128) DEFAULT NULL,
+  `Password` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`AccountNum`),
+  UNIQUE KEY `Token` (`Token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
+CREATE TABLE `Company` (
+  `AccountNum` varchar(128) NOT NULL,
+  `Name` varchar(128) DEFAULT NULL,
+  `TelNum` varchar(128) DEFAULT NULL,
+  `EmailAddress` varchar(128) DEFAULT NULL,
+  `Token` varchar(128) DEFAULT NULL,
+  `Credit` bigint(20) DEFAULT NULL,
+  `Status` varchar(128) DEFAULT 'unapplied',
+  PRIMARY KEY (`AccountNum`),
+  UNIQUE KEY `EmailAddress` (`EmailAddress`),
+  UNIQUE KEY `Token` (`Token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
-#
-# Structure for table "companyaddress"
-#
-
-DROP TABLE IF EXISTS `CompanyAddress`;
 CREATE TABLE `CompanyAddress` (
-  `AccountNum` varchar(128),
-  `Addr` varchar(128),
+  `AccountNum` varchar(128) NOT NULL,
+  `Addr` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`AccountNum`),
-  CONSTRAINT `companyaddress_ibfk_1` FOREIGN KEY (`AccountNum`) REFERENCES `Company` (`AccountNum`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT `companyaddress_ibfk_1` FOREIGN KEY (`AccountNum`) REFERENCES `company` (`accountnum`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
-#
-# Data for table "companyaddress"
-#
+CREATE TABLE `Credit` (
+  `AccountNum` varchar(128) NOT NULL,
+  `GuarantyId` int(11) NOT NULL,
+  `ReportId` int(11) NOT NULL,
+  `Type` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`AccountNum`,`GuarantyId`,`ReportId`),
+  KEY `credit_fk_guaranty` (`GuarantyId`),
+  KEY `credit_fk_report` (`ReportId`),
+  CONSTRAINT `credit_fk_company` FOREIGN KEY (`AccountNum`) REFERENCES `company` (`accountnum`),
+  CONSTRAINT `credit_fk_guaranty` FOREIGN KEY (`GuarantyId`) REFERENCES `guaranty` (`guarantyid`),
+  CONSTRAINT `credit_fk_report` FOREIGN KEY (`ReportId`) REFERENCES `report` (`reportid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
-
-#
-# Structure for table "fundamentalcertificate"
-#
-
-DROP TABLE IF EXISTS `FundamentalCertificate`;
 CREATE TABLE `FundamentalCertificate` (
-  `BusinessLicenceNum` varchar(128),
-  `StartDate` datetime,
-  `EndDate` datetime,
-  `BusinessRegistrationNo` varchar(128),
-  `RegisteredCapital` bigint(20),
-  `MainBusinessScope` varchar(128),
-  `OperationType` int(11),
-  `AccountNum` varchar(128),
+  `BusinessLicenceNum` varchar(128) DEFAULT NULL,
+  `StartDate` datetime DEFAULT NULL,
+  `EndDate` datetime DEFAULT NULL,
+  `BusinessRegistrationNo` varchar(128) DEFAULT NULL,
+  `RegisteredCapital` bigint(20) DEFAULT NULL,
+  `MainBusinessScope` varchar(128) DEFAULT NULL,
+  `OperationType` int(11) DEFAULT NULL,
+  `AccountNum` varchar(128) NOT NULL,
   PRIMARY KEY (`AccountNum`),
-  CONSTRAINT `fundamentalcertificate_ibfk_1` FOREIGN KEY (`AccountNum`) REFERENCES `Company` (`AccountNum`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT `fundamentalcertificate_ibfk_1` FOREIGN KEY (`AccountNum`) REFERENCES `company` (`accountnum`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
-#
-# Data for table "fundamentalcertificate"
-#
-
-
-#
-# Structure for table "report"
-#
-
-DROP TABLE IF EXISTS `Report`;
-CREATE TABLE `Report` (
-  `AccountNum` varchar(128),
-  `ReportId` int(11) AUTO_INCREMENT,
-  `Date` datetime,
-  `Duration` varchar(128),
-  PRIMARY KEY (`ReportId`),
-  KEY `AccountNum` (`AccountNum`),
-  CONSTRAINT `report_ibfk_1` FOREIGN KEY (`AccountNum`) REFERENCES `Authorization` (`AccountNum`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-#
-# Data for table "report"
-#
-
-
-
-
-DROP table if EXISTS `Guaranty`;
-CREATE TABLE `Guaranty`(
-  `GuarantyId` int(11) AUTO_INCREMENT,
-  `AccountNum` varchar(128),
-  `State` int(11),
-  `ScopeOfRight` int(11),
-  `OwnerName` varchar(128),
-  `ReportId` int(11),
+CREATE TABLE `Guaranty` (
+  `GuarantyId` int(11) NOT NULL AUTO_INCREMENT,
+  `AccountNum` varchar(128) DEFAULT NULL,
+  `State` int(11) DEFAULT NULL,
+  `ScopeOfRight` int(11) DEFAULT NULL,
+  `OwnerName` varchar(128) DEFAULT NULL,
+  `ReportId` int(11) DEFAULT NULL,
   `Type` int(11) DEFAULT '0',
-  `EvaluateValue` int(11),
-  `Name` varchar(128),
-  CONSTRAINT `guaranty_fk_company` FOREIGN KEY (`AccountNum`) REFERENCES `Company` (`AccountNum`),
-  CONSTRAINT `guaranty_fk_report` FOREIGN KEY (`ReportId`) REFERENCES `Report` (`ReportId`),
-  PRIMARY KEY(`GuarantyId`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-#
-# Structure for table "machine"
-#
-
-DROP TABLE IF EXISTS `Machine`;
-CREATE TABLE `Machine` (
-  `UsedDays` int(11),
-  `Producer` varchar(128),
-  `Model` varchar(128),
-  `GuarantyId` int(11),
+  `EvaluateValue` int(11) DEFAULT NULL,
+  `Name` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`GuarantyId`),
-  #KEY `reportId` (`reportId`),
-  #CONSTRAINT `machine_ibfk_1` FOREIGN KEY (`reportId`) REFERENCES `report` (`reportId`),
-  CONSTRAINT `machine_fk_guaranty` FOREIGN KEY (`GuarantyId`) REFERENCES `Guaranty` (`GuarantyId`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  KEY `guaranty_fk_company` (`AccountNum`),
+  KEY `guaranty_fk_report` (`ReportId`),
+  CONSTRAINT `guaranty_fk_company` FOREIGN KEY (`AccountNum`) REFERENCES `company` (`accountnum`),
+  CONSTRAINT `guaranty_fk_report` FOREIGN KEY (`ReportId`) REFERENCES `report` (`reportid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
-#
-# Data for table "machine"
-#
-
-
-#
-# Structure for table "land"
-#
-
-DROP TABLE IF EXISTS `Land`;
-CREATE TABLE `Land` (
-  `Addr` varchar(128),
-  `Area` bigint(20),
-  `GuarantyId` int(11),
-  PRIMARY KEY (`GuarantyId`),
-  #KEY `reportId` (`reportId`),
-  #CONSTRAINT `land_ibfk_1` FOREIGN KEY (`reportId`) REFERENCES `report` (`reportId`),
-  CONSTRAINT `land_fk_guaranty` FOREIGN KEY (`GuarantyId`) REFERENCES `Guaranty` (`GuarantyId`) ON DELETE CASCADE
-
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-#
-# Data for table "land"
-#
-
-
-#
-# Structure for table "house"
-#
-
-DROP TABLE IF EXISTS `House`;
 CREATE TABLE `House` (
-  `Addr` varchar(128),
+  `Addr` varchar(128) DEFAULT NULL,
   `Zip` varchar(128) DEFAULT NULL,
-  `HousingCertificatedId` varchar(128),
-  `GuarantyId` int(11),
+  `HousingCertificatedId` varchar(128) DEFAULT NULL,
+  `GuarantyId` int(11) NOT NULL,
   PRIMARY KEY (`GuarantyId`),
-  #KEY `reportId` (`reportId`),
-  #CONSTRAINT `house_ibfk_1` FOREIGN KEY (`reportId`) REFERENCES `report` (`reportId`),
-  CONSTRAINT `house_fk_guaranty` FOREIGN KEY (`GuarantyId`) REFERENCES `Guaranty` (`GuarantyId`) ON DELETE CASCADE
+  CONSTRAINT `house_fk_guaranty` FOREIGN KEY (`GuarantyId`) REFERENCES `guaranty` (`guarantyid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `Land` (
+  `Addr` varchar(128) DEFAULT NULL,
+  `Area` bigint(20) DEFAULT NULL,
+  `GuarantyId` int(11) NOT NULL,
+  PRIMARY KEY (`GuarantyId`),
+  CONSTRAINT `land_fk_guaranty` FOREIGN KEY (`GuarantyId`) REFERENCES `guaranty` (`guarantyid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
-#
-# Data for table "house"
-#
-
-
-#
-# Structure for table "securitiesinformation"
-#
-
-DROP TABLE IF EXISTS `Securitiesinformation`;
-CREATE TABLE `Securitiesinformation` (
-  `AccountNum` varchar(128),
-  `StockCode` varchar(128),
-  `ListedApproval` varchar(128),
-  `StockAbbr` varchar(128),
-  `IssuePrice` bigint(20) DEFAULT NULL,
-  `StockType` int(11),
-  `LaunchDate` datetime,
-  `Circulation` bigint(20),
-  `IsValid` tinyint(4),
-  `Remark` varchar(128) DEFAULT NULL,
-  PRIMARY KEY (`AccountNum`),
-  CONSTRAINT `securitiesinformation_ibfk_1` FOREIGN KEY (`AccountNum`) REFERENCES `Company` (`AccountNum`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-#
-# Data for table "securitiesinformation"
-#
-
-
-
-
-
-
-
-DROP TABLE IF EXISTS `Credit`;
-CREATE TABLE `Credit`(
-  `AccountNum` varchar(128),
-  `GuarantyId` int(11),
-  `ReportId` int(11),
-  `Type` tinyint(1),
-  PRIMARY KEY(`AccountNum`,`GuarantyId`,`ReportId`),
-  CONSTRAINT `credit_fk_guaranty` FOREIGN KEY (`GuarantyId`) REFERENCES `Guaranty` (`GuarantyId`),
-  CONSTRAINT `credit_fk_company` FOREIGN KEY (`AccountNum`) REFERENCES `Company` (`AccountNum`),
-  CONSTRAINT `credit_fk_report` FOREIGN KEY (`ReportId`) REFERENCES `Report` (`ReportId`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-CREATE TABLE IF NOT EXISTS `Protocol` (
-  `ProtocolId` INT(11) NOT NULL AUTO_INCREMENT,
-  `GurantyId` INT(11) NOT NULL,
-  `StartDate` DATE NOT NULL,
-  `Duration` VARCHAR(128) NOT NULL,
-  `EndDate` DATE NOT NULL,
-  `Message` MEDIUMTEXT NULL,
-  `Condition` VARCHAR(45) NULL,
-  PRIMARY KEY (`ProtocolId`),
-  CONSTRAINT `fk_protocol_to_guranty`
-  FOREIGN KEY (`GurantyId`)
-  REFERENCES `Guaranty` (`GuarantyId`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-  ENGINE = InnoDB;
+CREATE TABLE `Machine` (
+  `UsedDays` int(11) DEFAULT NULL,
+  `Producer` varchar(128) DEFAULT NULL,
+  `Model` varchar(128) DEFAULT NULL,
+  `GuarantyId` int(11) NOT NULL,
+  PRIMARY KEY (`GuarantyId`),
+  CONSTRAINT `machine_fk_guaranty` FOREIGN KEY (`GuarantyId`) REFERENCES `guaranty` (`guarantyid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
 CREATE TABLE `Message` (
   `id` int(11) NOT NULL,
@@ -276,3 +113,43 @@ CREATE TABLE `Message` (
   `status` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
+CREATE TABLE `Protocol` (
+  `ProtocolId` int(11) NOT NULL AUTO_INCREMENT,
+  `GurantyId` int(11) NOT NULL,
+  `StartDate` date NOT NULL,
+  `Duration` varchar(128) NOT NULL,
+  `EndDate` date NOT NULL,
+  `Message` mediumtext,
+  `Condition` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`ProtocolId`),
+  KEY `fk_protocol_to_guranty` (`GurantyId`),
+  CONSTRAINT `fk_protocol_to_guranty` FOREIGN KEY (`GurantyId`) REFERENCES `guaranty` (`guarantyid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
+CREATE TABLE `Report` (
+  `AccountNum` varchar(128) DEFAULT NULL,
+  `ReportId` int(11) NOT NULL AUTO_INCREMENT,
+  `Date` datetime DEFAULT NULL,
+  `Duration` varchar(128) DEFAULT NULL,
+  `type` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`ReportId`),
+  KEY `AccountNum` (`AccountNum`),
+  CONSTRAINT `report_ibfk_1` FOREIGN KEY (`AccountNum`) REFERENCES `authorization` (`accountnum`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
+CREATE TABLE `Securitiesinformation` (
+  `AccountNum` varchar(128) NOT NULL,
+  `StockCode` varchar(128) DEFAULT NULL,
+  `ListedApproval` varchar(128) DEFAULT NULL,
+  `StockAbbr` varchar(128) DEFAULT NULL,
+  `IssuePrice` bigint(20) DEFAULT NULL,
+  `StockType` int(11) DEFAULT NULL,
+  `LaunchDate` datetime DEFAULT NULL,
+  `Circulation` bigint(20) DEFAULT NULL,
+  `IsValid` tinyint(4) DEFAULT NULL,
+  `Remark` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`AccountNum`),
+  CONSTRAINT `securitiesinformation_ibfk_1` FOREIGN KEY (`AccountNum`) REFERENCES `company` (`accountnum`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
