861df938728a3f31a512184dbab51e971f3e6f3b
enderqiu <a934560824@163.com>
2018-07-16 16:42:10 +0800

    request a interface

--- src/main/java/cn/ssyram/blockchain/interfaces/GuarantyChain.java
+++ src/main/java/cn/ssyram/blockchain/interfaces/GuarantyChain.java
@@ -2,6 +2,14 @@ package cn.ssyram.blockchain.interfaces;
 
 import cn.ssyram.blockchain.impls.GurantyChainImpl;
 
+import java.util.ArrayList;
+
 public interface GuarantyChain extends BlockChain {
     GuarantyChain chain = new GurantyChainImpl();
+
+    void updateGuarantyState(Integer guarantyId, Integer stateWillUpdateTo);
+
+    Integer getGuarantyState(Integer guarantyId);
+
+    ArrayList<Integer> getGuarantyState(ArrayList<Integer> guarantyIds);
 }
