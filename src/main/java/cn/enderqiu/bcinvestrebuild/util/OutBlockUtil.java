30be95fc8b5fd2c61d98a0f7422a3b1900a4289e
enderqiu <a934560824@163.com>
2018-07-17 16:56:05 +0800

    出块通知工具

---
+++ src/main/java/cn/enderqiu/bcinvestrebuild/util/OutBlockUtil.java
@@ -0,0 +1,10 @@
+package cn.enderqiu.bcinvestrebuild.util;
+
+import java.util.ArrayList;
+
+public class OutBlockUtil {
+
+    public static void notifyOutBlock(ArrayList<String> addresses){
+        return;
+    }
+}
