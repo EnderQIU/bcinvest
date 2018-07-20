3c4eca9d0a49f5cdaead6c22ac436fbabe724a6f
enderqiu <a934560824@163.com>
2018-07-20 15:26:16 +0800

    大小写写错

--- src/main/java/cn/enderqiu/bcinvestrebuild/util/GuarantyChainUtil.java
+++ src/main/java/cn/enderqiu/bcinvestrebuild/util/GuarantyChainUtil.java
@@ -172,7 +172,7 @@ public class GuarantyChainUtil {
                 dsl.update(Guaranty.GUARANTY)
                         .set(Guaranty.GUARANTY.STATE, record.getValue("stateWillUpdateTo", Integer.class))
                         .set(Guaranty.GUARANTY.LOCK, unlocked)
-                        .where(Guaranty.GUARANTY.GUARANTYID.eq(record.getValue("guarantyid", Integer.class))).execute();
+                        .where(Guaranty.GUARANTY.GUARANTYID.eq(record.getValue("guarantyId", Integer.class))).execute();
                 // 判断是否需要更新信用信息，若需要，则调用 CreditChainUtil.updateCredit()
                 int creditNeedToBeUpdated = GuarantyChainUtil.checkHowMuchCreditNeedToBeUpdated(record.getValue("previousState", Integer.class), stateWillUpdateTo);
                 if ( creditNeedToBeUpdated != 0){
