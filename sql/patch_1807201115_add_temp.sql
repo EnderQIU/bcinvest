44b4497b3a0f0016524d7cb0ef4d0e5f2237bda7
chenchen <709313510@qq.com>
2018-07-20 11:15:43 +0800

    新增临时表 用于分页

---
+++ sql/patch_1807201115_add_temp.sql
@@ -0,0 +1,20 @@
+﻿# Host: localhost  (Version 5.7.16-log)
+# Date: 2018-07-20 10:37:17
+# Generator: MySQL-Front 6.0  (Build 2.20)
+
+
+#
+# Structure for table "temp"
+#
+
+DROP TABLE IF EXISTS `temp`;
+CREATE TABLE `temp` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `GuarantyId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "temp"
+#
+
